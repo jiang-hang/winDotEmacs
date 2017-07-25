a2d3494f080d89c1924bc4b0e0064a5f4c617842
jianghang <jianghang@bagualu.net>
2017-07-26 07:46:21 +0800

    update for blog post

--- msync.sh
+++ msync.sh
@@ -1,15 +1,22 @@
 #! /usr/bin/bash
 
+echo "ad Conv Sync"
+
 cd ~/blog/html
 
 ff=`ls -1 *.html`
 
 for mf in $ff
 do
+    utf=`enca -L zh_CN $mf | grep -i UTF`
+    if [ -z "$utf" ] ; then
     iconv  -f gbk -t utf-8  $mf > tt.utf8
     mv tt.utf8 $mf
     echo "convert $mf is done"
+    fi
 done
+
+chmod 644 *.html
 
 rsync -avz *.html  wp.bagualu.net:/var/www/bagualu/markdown/
 
